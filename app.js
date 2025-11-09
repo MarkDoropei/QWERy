b76d5b8d26b158bbe81079436deaacc888256ce3
Your Name <you@example.com>
2025-11-09 21:47:19 +0300

    New update

--- app.js
+++ app.js
@@ -4,3 +4,35 @@
         prevEl: ".swiper-button-prev",
       },
     });
+
+function highlightWord() {
+
+  const word = document.getElementById('searchInput').value.trim();
+  if (!word) return;
+
+  const body = document.body;
+
+  const regex = new RegExp(`(${word})`, 'gi');
+
+  function traverse(node) {
+    if (node.nodeType === 3) {
+      if (regex.test(node.nodeValue)) {
+        const span = document.createElement('span');
+        span.innerHTML = node.nodeValue.replace(regex, '<mark>$1</mark>');
+        node.parentNode.replaceChild(span, node);
+      }
+    } else if (node.nodeType === 1 && node.tagName !== 'SCRIPT' && node.tagName !== 'STYLE') {
+      for (let i = 0; i < node.childNodes.length; i++) {
+        traverse(node.childNodes[i]);
+      }
+    }
+  }
+  traverse(body);
+}
+
+function clearHighlights() {
+  document.querySelectorAll('mark').forEach(mark => {
+    const parent = mark.parentNode;
+    parent.replaceChild(document.createTextNode(mark.textContent), mark);
+  });
+}
